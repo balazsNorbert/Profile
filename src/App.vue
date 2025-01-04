494e0d0b1ca547a9b3d10f6f4c53606b5ffa7570
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2025-01-04 16:47:33 +0200

    Starting files

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'My Profile',
+    };
+  },
+};
+</script>
+
+<template>
+  <h1 class="text-2xl text-red-400 bg-black">{{ name }}</h1>
+</template>
