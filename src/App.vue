538871721043d09325cc5fbb88083f2e874c43fa
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2025-01-07 18:25:20 +0200

    Add first contacts

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,13 @@ export default {
 </script>
 
 <template>
-  <div class="bg-bgImage bg-cover min-h-screen relative">
+  <div class="bg-bgImage bg-cover min-h-screen w-full relative">
     <Navbar />
-    <div class="absolute top-24 m-10 gap-10 md:gap-32 flex flex-col md:flex-row justify-between items-center">
-      <img class="max-w-70 md:max-w-80 xl:max-w-96 bg-white rounded-full p-4 border-separated border-4 border-sky-300" src="./assets/png/profile-character.png" alt="Profile picture">
-      <p class="font-lexend text-white text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold hover:scale-110">I am a web developer and frontend developer with a passion for building beautiful and responsive web applications.</p>
+    <div class="absolute top-24 m-10 gap-10  xl:gap-32 flex flex-col md:flex-row justify-between items-center">
+      <img class="max-w-70 md:max-w-80 xl:max-w-96 bg-white rounded-full p-4 border-separated border-4 border-sky-300 hover:scale-110" src="./assets/png/profile-character.png" alt="Profile picture">
+      <p class="font-lexend text-white text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold">I am a web developer and frontend developer with a passion for building beautiful and responsive web applications.</p>
     </div>
-    <Contacts />
+    <Contact />
     <RouterView />
   </div>
 </template>
--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Contacts</h1>
+    <div class="w-full absolute bottom-28 text-white flex justify-center">
+        <ul class="mt-4 flex justify-between gap-10 text-2xl">
+            <li class="flex items-center gap-3">
+                <span class="material-icons text-white md-60">
+                    phone
+                </span>
+                <p>+123 456 789</p>
+            </li>
+            <li class="flex items-center gap-3">
+                <span class="material-icons-outlined text-white md-60">
+                    email
+                </span>
+                <p>+123 456 789</p>
+            </li>
+            <li class="flex items-center gap-3">
+                <img class="w-14 h-14 text-white" src="../assets/svg/linkedin-brands-solid.svg" alt="Linkedin image">
+                <p>+123 456 789</p>
+            </li>
+        </ul>
+    </div>
 </template>
