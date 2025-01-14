eb50256537b74e8bfa9af02293efa592ec7ce50d
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2025-01-14 15:41:24 +0200

    Update Vue Router to use createWebHistory for correct URL handling.

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
 <template>
   <div class="flex flex-col bg-bgImage bg-cover min-h-screen w-full relative">
     <Navbar />
-    <main class="m-10 gap-10 xl:gap-32 flex flex-col md:flex-row md:justify-between flex-grow items-center">
+    <main class="m-10 gap-10 xl:gap-20 flex flex-col md:flex-row md:justify-between flex-grow items-center">
       <img loading="lazy" class="max-w-72 md:max-w-80 xl:max-w-96 bg-white rounded-full p-4 border-separated border-4 border-sky-300 hover:scale-110 ease-in-out duration-500" src="./assets/png/profile-character.png" alt="Profile picture">
       <div class="text-center md:text-left">
         <p loading="lazy" class="font-lexend text-white text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold animate-fade-in-down">
