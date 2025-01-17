b5033d451d854d42c7c00af437908de17f91e31c
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2025-01-17 12:34:20 +0200

    Fix the CV URL and get rid of some AOS animation for better user experience on mobile.

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -4,137 +4,142 @@ import 'aos/dist/aos.css';
 export default {
   mounted() {
     AOS.init({
-        duration: 500,
-        delay: 200,
+      duration: 500,
     });
   }
 };
 </script>
 <template>
-    <div class="flex flex-col items-center gap-14 py-12 bg-sky-200 font-lexend text-sky-700 text-lg md:text-xl xl:text-2xl">
-        <h2 data-aos="fade-up" class="text-3xl md:text-4xl xl:text-5xl font-semibold text-center text-sky-900">Portfolio</h2>
-        <div data-aos="fade-up" class="flex items-center text-left w-5/6 gap-2 pl-6">
-            <span class="material-icons md-36">
-                work
-            </span>
-            <h3 class="text-2xl md:text-3xl xl:text-4xl">Internship:</h3>
+  <div class="bg-sky-200 font-lexend text-sky-700 text-sm md:text-xl xl:text-2xl">
+    <div class="w-5/6 mx-auto flex flex-col items-center gap-14 py-12">
+      <h2 class="text-3xl md:text-4xl xl:text-5xl font-semibold text-center text-sky-900">Portfolio</h2>
+      <div class="flex items-center text-left w-full gap-2">
+        <span class="material-icons md-36">
+          work
+        </span>
+        <h3 class="text-2xl md:text-3xl xl:text-4xl">Internship:</h3>
+      </div>
+      <div class="flex flex-col text-left w-full gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+        <p>
+          Developed a <span class="font-bold">full-stack</span> web application using <span
+            class="font-bold">TALL(Tailwind,
+            Alpinge.js, Laravel, Livewire) stack</span> based on designs from Figma.
+        </p>
+        <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2"
+          href="https://bitbucket.org/mencs2022/mencs-repo-ll/" target="_blank">
+          <span>Tall stack project</span>
+          <span class="material-icons-outlined ">
+            east
+          </span>
+        </a>
+      </div>
+      <h3 data-aos="fade-up"
+        class="flex items-center flex-wrap gap-2 text-left w-full text-2xl md:text-3xl xl:text-4xl">
+        <div class="flex items-center gap-2">
+          <span class="material-icons md-36">school</span>
+          <span>Certifications & </span>
         </div>
-        <div data-aos="fade-up" class="flex flex-col w-5/6 gap-4 bg-white p-6 rounded-3xl shadow-2xl">
-            <p>
-                Developed a <span class="font-bold">full-stack</span> web application using <span class="font-bold">TALL(Tailwind,
-                Alpinge.js, Laravel, Livewire) stack</span> based on designs from Figma.
-            </p>
-            <a class="border-2 border-sky-700 focus:border-white
-          hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2" href="https://bitbucket.org/mencs2022/mencs-repo-ll/" target="_blank">
-                <span>Tall stack project</span>
-                <span class="material-icons-outlined ">
-                    east
-                </span>
+        <div class="flex items-center gap-2">
+          <span class="material-icons md-36">folder</span>
+          <span>Projects:</span>
+        </div>
+      </h3>
+      <div data-aos="fade-up" class="flex flex-col gap-8 text-left w-full">
+        <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+          <h3 class="text-xl md:text-2xl xl:text-3xl">
+            THE COMPLETE 2023 WEB DEVELOPMENT BOOTCAMP
+          </h3>
+          <div class="flex gap-5 flex-wrap">
+            <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit
+                        flex items-center gap-2"
+              href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
+              <span>View Certificate</span>
+              <span class="material-icons md-36">visibility</span>
             </a>
+            <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit
+                        flex items-center gap-2" href="https://github.com/balazsNorbert/Keeper-App" target="_blank">
+              <span>React project</span>
+              <span class="material-icons-outlined ">
+                east
+              </span>
+            </a>
+            <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+                        items-center gap-2" href="https://github.com/balazsNorbert/To-Do-List" target="_blank">
+              <span>ToDo List project</span>
+              <span class="material-icons-outlined ">
+                east
+              </span>
+            </a>
+            <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+                        items-center gap-2" href="https://github.com/balazsNorbert/Blog-Project" target="_blank">
+              <span>Blog project</span>
+              <span class="material-icons-outlined ">
+                east
+              </span>
+            </a>
+          </div>
         </div>
-        <h3 data-aos="fade-up" class="flex items-center gap-2 text-left w-5/6 text-2xl md:text-3xl xl:text-4xl pl-6">
-            <span class="material-icons md-36">school</span>
-            <span>Certifications & </span>
-            <span class="material-icons md-36">folder</span>
-            <span>Projects:</span>
-        </h3>
-        <div data-aos="fade-up" class="flex flex-col w-5/6 gap-8">
-            <div class="flex flex-col gap-4">
-                <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
-                    <h3 class="text-xl md:text-2xl xl:text-3xl">
-                        THE COMPLETE 2023 WEB DEVELOPMENT BOOTCAMP
-                    </h3>
-                    <div class="flex gap-5 flex-wrap">
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2"
-                        href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
-                            <span>View Certificate</span>
-                            <span class="material-icons md-36">visibility</span>
-                        </a>
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2" href="https://github.com/balazsNorbert/Keeper-App" target="_blank">
-                            <span>React project</span>
-                            <span class="material-icons-outlined ">
-                                east
-                            </span>
-                        </a>
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2" href="https://github.com/balazsNorbert/To-Do-List" target="_blank">
-                            <span>ToDo List project</span>
-                            <span class="material-icons-outlined ">
-                                east
-                            </span>
-                        </a>
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2" href="https://github.com/balazsNorbert/Blog-Project" target="_blank">
-                            <span>Blog project</span>
-                            <span class="material-icons-outlined ">
-                                east
-                            </span>
-                        </a>
-                    </div>
-                </div>
+        <div data-aos="fade-up" class="flex flex-col gap-4">
+          <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+            <h3 class="text-xl md:text-2xl xl:text-3xl">
+              Angular - The Complete Guide (2023 Edition)
+            </h3>
+            <div class="flex gap-5 flex-wrap">
+              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+                        items-center gap-2"
+                href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
+                <span>View Certificate</span>
+                <span class="material-icons md-36">visibility</span>
+              </a>
+              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+                        items-center gap-2" href="https://github.com/balazsNorbert/Angular-Project" target="_blank">
+                <span>Angular project</span>
+                <span class="material-icons-outlined">
+                  east
+                </span>
+              </a>
             </div>
-            <div data-aos="fade-up" class="flex flex-col gap-4">
-                <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
-                    <h3 class="text-xl md:text-2xl xl:text-3xl">
-                        Angular - The Complete Guide (2023 Edition)
-                    </h3>
-                    <div class="flex gap-5 flex-wrap">
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2"
-                        href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
-                            <span>View Certificate</span>
-                            <span class="material-icons md-36">visibility</span>
-                        </a>
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2" href="https://github.com/balazsNorbert/Angular-Project" target="_blank">
-                            <span>Angular project</span>
-                            <span class="material-icons-outlined">
-                                east
-                            </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div data-aos="fade-up" class="flex flex-col gap-4">
-                <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
-                    <h3 class="text-xl md:text-2xl xl:text-3xl">
-                        Understanding TypeScript
-                    </h3>
-                    <div class="flex gap-5 flex-wrap">
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2"
-                        href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
-                            <span>View Certificate</span>
-                            <span class="material-icons md-36">visibility</span>
-                        </a>
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2" href="https://github.com/balazsNorbert/Typescript-project" target="_blank">
-                            <span>TypeScript project</span>
-                            <span class="material-icons-outlined">
-                                east
-                            </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div data-aos="fade-up" class="flex flex-col gap-4">
-                <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
-                    <h3 class="text-xl md:text-2xl xl:text-3xl">
-                        My Profile website (Vue.js)
-                    </h3>
-                    <div class="flex gap-5 flex-wrap">
-                        <a class="border-2 border-sky-700 focus:border-white
-                        hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex items-center gap-2" href="https://github.com/balazsNorbert/Profile" target="_blank">
-                            <span>Profile project</span>
-                            <span class="material-icons-outlined">
-                                east
-                            </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+        <div data-aos="fade-up" class="flex flex-col gap-4">
+          <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+            <h3 class="text-xl md:text-2xl xl:text-3xl">
+              Understanding TypeScript
+            </h3>
+            <div class="flex gap-5 flex-wrap">
+              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit
+                flex items-center gap-2" href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
+                <span>View Certificate</span>
+                <span class="material-icons md-36">visibility</span>
+              </a>
+              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit
+                flex items-center gap-2" href="https://github.com/balazsNorbert/Typescript-project"
+                target="_blank">
+                <span>TypeScript project</span>
+                <span class="material-icons-outlined">
+                  east
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div data-aos="fade-up" class="flex flex-col gap-4">
+          <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+            <h3 class="text-xl md:text-2xl xl:text-3xl">
+              My Profile website (Vue.js)
+            </h3>
+            <div class="flex gap-5 flex-wrap">
+              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+              items-center gap-2" href="https://github.com/balazsNorbert/Profile" target="_blank">
+                <span>Profile project</span>
+                <span class="material-icons-outlined">
+                  east
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,22 +1,9 @@
-<script>
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-export default {
-  mounted() {
-    AOS.init({
-        duration: 500,
-        delay: 200,
-    });
-  }
-};
-</script>
 <template>
-  <div class="flex flex-col items-center gap-3 mb-14">
-    <h2 data-aos="fade-in" class="font-lexend text-white text-3xl md:text-4xl xl:text-5xl mb-6">My Resume:</h2>
-    <div data-aos="fade-in" class="flex flex-wrap text-center ml-10 gap-4 text-md md:text-xl xl:text-2xl">
+  <div class="flex flex-col items-center gap-3 mb-14 w-5/6 mx-auto">
+    <h2 class="font-lexend text-white text-3xl md:text-4xl xl:text-5xl mb-6">My Resume:</h2>
+    <div class="flex flex-wrap text-center gap-4 text-md md:text-xl xl:text-2xl">
       <a
-        href="/Balázs_Norbert_en.pdf"
+        href="/Profile/Balazs_Norbert_en.pdf"
         target="_blank"
         class="bg-gradient-to-r from-blue-600 via-cyan-500 to-teal-500 text-white py-3 px-4 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-xl hover:from-blue-600 hover:to-teal-600">
         <span class="flex items-center gap-2">
@@ -25,7 +12,7 @@ export default {
         </span>
       </a>
       <a
-        href="/Balázs_Norbert_en.pdf"
+        href="/Profile/Balazs_Norbert_en.pdf"
         download="Balázs_Norbert_en.pdf"
         class="bg-gradient-to-r to-green-500 via-lime-500 from-yellow-500 text-white py-3 px-4 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-xl hover:to-green-600 hover:from-yellow-600">
         <span class="flex items-center gap-2">
