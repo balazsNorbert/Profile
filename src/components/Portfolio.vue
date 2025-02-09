b2569d8930b405ec3dcac5eaf8fb86914dd4503f
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2025-02-09 14:39:15 +0200

    Add a new project to my portfolio.

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -43,8 +43,45 @@ export default {
           <span>Projects:</span>
         </div>
       </h3>
-      <div data-aos="fade-up" class="flex flex-col gap-8 text-left w-full">
-        <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+      <div class="flex flex-col gap-8 text-left w-full">
+        <div data-aos="fade-up" class="flex flex-col gap-4">
+          <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+            <h3 class="text-lg md:text-xl xl:text-2xl">
+              Workout Tracker website (React.js, Node.js, MongoDB)
+            </h3>
+            <div class="flex gap-5 flex-wrap">
+              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+              items-center gap-2" href="https://workoutracker.com" target="_blank">
+                <span>View website</span>
+                <span class="material-icons md-36">visibility</span>
+              </a>
+              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+              items-center gap-2" href="https://github.com/balazsNorbert/PR-tracker" target="_blank">
+                <span>Workout tracker {{ $t( 'project' ) }}</span>
+                <span class="material-icons-outlined">
+                  east
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div data-aos="fade-up" class="flex flex-col gap-4">
+          <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+            <h3 class="text-lg md:text-xl xl:text-2xl">
+              My Profile website (Vue.js)
+            </h3>
+            <div class="flex gap-5 flex-wrap">
+              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+              items-center gap-2" href="https://github.com/balazsNorbert/Profile" target="_blank">
+                <span>Profile {{ $t( 'project' ) }}</span>
+                <span class="material-icons-outlined">
+                  east
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div data-aos="fade-up" class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
           <h3 class="text-lg md:text-xl xl:text-2xl">
             THE COMPLETE 2023 WEB DEVELOPMENT BOOTCAMP
           </h3>
@@ -79,66 +116,46 @@ export default {
             </a>
           </div>
         </div>
-        <div data-aos="fade-up" class="flex flex-col gap-4">
-          <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
-            <h3 class="text-lg md:text-xl xl:text-2xl">
-              Angular - The Complete Guide (2023 Edition)
-            </h3>
-            <div class="flex gap-5 flex-wrap">
-              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
-                        items-center gap-2"
-                href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
-                <span>{{ $t( 'viewCertificate' ) }}</span>
-                <span class="material-icons md-36">visibility</span>
-              </a>
-              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
-                        items-center gap-2" href="https://github.com/balazsNorbert/Angular-{{ $t('project') }}"
-                target="_blank">
-                <span>Angular {{ $t( 'project' ) }}</span>
-                <span class="material-icons-outlined">
-                  east
-                </span>
-              </a>
-            </div>
+        <div data-aos="fade-up" class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+          <h3 class="text-lg md:text-xl xl:text-2xl">
+            Angular - The Complete Guide (2023 Edition)
+          </h3>
+          <div class="flex gap-5 flex-wrap">
+            <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+                      items-center gap-2"
+              href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
+              <span>{{ $t( 'viewCertificate' ) }}</span>
+              <span class="material-icons md-36">visibility</span>
+            </a>
+            <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
+                      items-center gap-2" href="https://github.com/balazsNorbert/Angular-{{ $t('project') }}"
+              target="_blank">
+              <span>Angular {{ $t( 'project' ) }}</span>
+              <span class="material-icons-outlined">
+                east
+              </span>
+            </a>
           </div>
         </div>
-        <div data-aos="fade-up" class="flex flex-col gap-4">
-          <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
-            <h3 class="text-lg md:text-xl xl:text-2xl">
-              Understanding TypeScript
-            </h3>
-            <div class="flex gap-5 flex-wrap">
-              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit
-                flex items-center gap-2"
-                href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
-                <span>{{ $t( 'viewCertificate' ) }}</span>
-                <span class="material-icons md-36">visibility</span>
-              </a>
-              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit
-                flex items-center gap-2" href="https://github.com/balazsNorbert/Typescript-{{ $t('project') }}"
-                target="_blank">
-                <span>TypeScript {{ $t( 'project' ) }}</span>
-                <span class="material-icons-outlined">
-                  east
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div data-aos="fade-up" class="flex flex-col gap-4">
-          <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
-            <h3 class="text-lg md:text-xl xl:text-2xl">
-              My Profile website (Vue.js)
-            </h3>
-            <div class="flex gap-5 flex-wrap">
-              <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit flex
-              items-center gap-2" href="https://github.com/balazsNorbert/Profile" target="_blank">
-                <span>Profile {{ $t( 'project' ) }}</span>
-                <span class="material-icons-outlined">
-                  east
-                </span>
-              </a>
-            </div>
+        <div class="flex flex-col gap-4 bg-white p-6 rounded-3xl shadow-2xl">
+          <h3 class="text-lg md:text-xl xl:text-2xl">
+            Understanding TypeScript
+          </h3>
+          <div class="flex gap-5 flex-wrap">
+            <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit
+              flex items-center gap-2"
+              href="https://www.udemy.com/certificate/UC-ca1032bc-daab-47a9-b8d6-72fd527a87af/" target="_blank">
+              <span>{{ $t( 'viewCertificate' ) }}</span>
+              <span class="material-icons md-36">visibility</span>
+            </a>
+            <a class="border-2 border-sky-700 hover:bg-sky-700 hover:text-white p-3 rounded-full w-fit
+              flex items-center gap-2" href="https://github.com/balazsNorbert/Typescript-{{ $t('project') }}"
+              target="_blank">
+              <span>TypeScript {{ $t( 'project' ) }}</span>
+              <span class="material-icons-outlined">
+                east
+              </span>
+            </a>
           </div>
         </div>
       </div>
